a586837d464d049e84a93d87fb65f69b8bc63627
d0kuzu <dkobdabaev@mail.ru>
2025-12-02 12:50:28 +0500

    updated function tools

--- constants/ai_tools.go
+++ constants/ai_tools.go
@@ -6,69 +6,22 @@ var Tools = []openai.Tool{
 	{
 		Type: openai.ToolTypeFunction,
 		Function: &openai.FunctionDefinition{
-			Name:        "get_menu_queries",
-			Description: "Извлекает запросы к меню, которые нужно проверить в векторной базе",
+			Name:        "bookcampussanjose",
+			Description: "Use this function only once and only after the customer has explicitly confirmed their interest in booking a campus tour in San Jose.\n\nOnce the confirmation is received, trigger the function to return the appointment booking link.",
 			Parameters: map[string]interface{}{
-				"type": "object",
-				"properties": map[string]interface{}{
-					"items": map[string]interface{}{
-						"type": "array",
-						"items": map[string]interface{}{
-							"type": "object",
-							"properties": map[string]interface{}{
-								"query": map[string]interface{}{
-									"type":        "string",
-									"description": "Что именно ищет пользователь (например: пицца, хинкали, сок)",
-								},
-								"category": map[string]interface{}{
-									"type":        "string",
-									"description": "Категория блюда (например: Соусы, Основные блюда, Закуски, Комбо сеты и т.д.)",
-								},
-							},
-							"required": []string{"query", "category"},
-						},
-					},
-				},
-				"required": []string{"items"},
+				"type":       "object",
+				"properties": map[string]interface{}{},
 			},
 		},
 	},
 	{
 		Type: openai.ToolTypeFunction,
 		Function: &openai.FunctionDefinition{
-			Name:        "set_order",
-			Description: "Фиксирует заказ клиента по выбранным позициям меню, вызывается только когда заказ 100% окончен",
+			Name:        "bookcampussanfrancisco",
+			Description: "Use this function only once and only after the customer has explicitly confirmed their interest in booking a campus tour in San Francisco.\n\nOnce the confirmation is received, trigger the function to return the appointment booking link.",
 			Parameters: map[string]interface{}{
-				"type": "object",
-				"properties": map[string]interface{}{
-					"items": map[string]interface{}{
-						"type":        "array",
-						"description": "Список заказанных клиентом блюд",
-						"items": map[string]interface{}{
-							"type": "object",
-							"properties": map[string]interface{}{
-								"name": map[string]interface{}{
-									"type":        "string",
-									"description": "Название блюда или позиции из меню",
-								},
-								"description": map[string]interface{}{
-									"type":        "string",
-									"description": "Описание блюда или позиции из меню (если нет то продублировать название)",
-								},
-								"price": map[string]interface{}{
-									"type":        "integer",
-									"description": "Цена одной порции блюда в тенге",
-								},
-								"amount": map[string]interface{}{
-									"type":        "integer",
-									"description": "Количество порций, заказанных клиентом",
-								},
-							},
-							"required": []string{"name", "description", "price", "amount"},
-						},
-					},
-				},
-				"required": []string{"items"},
+				"type":       "object",
+				"properties": map[string]interface{}{},
 			},
 		},
 	},
