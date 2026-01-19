e1c860e3cc375b25a8c971fb8f6292222917bb00
d0kuzu <dkobdabaev@mail.ru>
2026-01-19 17:37:28 +0500

    improved proxy

--- api-gateway/routes/routes.go
+++ api-gateway/routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"api-gateway/config"
+	"api-gateway/middleware/auth"
 	"api-gateway/middleware/logger"
 	"api-gateway/proxy"
 	"github.com/gin-gonic/gin"
@@ -9,13 +10,14 @@ import (
 
 func SetupRoutes(r *gin.Engine, cfg *config.Config) {
 	r.Use(gin.LoggerWithFormatter(logger.Formatter))
+	r.Use(auth.AuthMiddleware())
 
 	r.GET("/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{"status": "ok"})
 	})
 
 	users := r.Group("/users")
-	users.Any("/*any", proxy.NewReverseProxy(cfg.UserServiceURL))
+	users.Any("/*any", proxy.NewReverseProxy(cfg.UserServiceURL, "/users"))
 
 	// Здесь можно добавить аггрегацию запросов позже, например:
 	// r.GET("/dashboard", aggregateHandler)
