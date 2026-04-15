4f2008379403d5c2a88826c030bde9e74fa2c3ca
d0kuzu <dkobdabaev@mail.ru>
2026-04-15 17:41:43 +0500

    chore: delete unnecessary directories

--- telegram-service/telegram/misc/keyboards.py
+++
@@ -1,9 +0,0 @@
-from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
-from telegram.misc.texts import TEXTS
-
-
-class InlineKeyboards:
-    pass
-
-class ReplyKeyboards:
-    pass
--- telegram-service/telegram/misc/states.py
+++
@@ -1,3 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-
